8cab97f90891e2d2a61b75a7da8c98df7e09f0f9
adrianobovolent <34462906+adrianobovolent@users.noreply.github.com>
2021-06-08 10:32:43 -0300

    Initial commit

---
+++ src/test/java/com/springfirstproject/SpringFirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

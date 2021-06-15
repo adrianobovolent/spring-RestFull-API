9da520a00c00650c636956b728cd2a6194fab53f
adrianobovolent <34462906+adrianobovolent@users.noreply.github.com>
2021-06-15 13:18:30 -0300

    Created RequestRepository Tests

--- src/main/java/com/springfirstproject/repository/RequestRepository.java
+++ src/main/java/com/springfirstproject/repository/RequestRepository.java
@@ -2,9 +2,12 @@ package com.springfirstproject.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springfirstproject.domain.Request;
 import com.springfirstproject.domain.enuns.RequestState;
@@ -13,7 +16,9 @@ import com.springfirstproject.domain.enuns.RequestState;
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
 	public List<Request> findAllByOwnerId(Long id);
-	
+
+	@Transactional(readOnly = false)
+	@Modifying
 	@Query("UPDATE request SET state = ?2 WHERE id = ?1")
-	public Request updateStatus(Long id, RequestState state);
+	public int updateStatus(Long id, RequestState state);
 }
--- src/test/java/com/springfirstproject/repository/RequestRepositoryTests.java
+++ src/test/java/com/springfirstproject/repository/RequestRepositoryTests.java
@@ -1,8 +1,13 @@
 package com.springfirstproject.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +26,67 @@ public class RequestRepositoryTests {
 	
 	@Autowired private RequestRepository requestRepository;
 	
-	public void saveTest() {
+	@Test
+	public void AsaveTest() {
 		User owner = new User();
-		owner.setId(1L);
+		owner.setId(6L);
 		Request request = new Request(null, "Novo LapTop HP","Pretendo obter um novo LapTop HP", new Date(), RequestState.OPEN, owner, null);
 		
-		requestRepository.save(request);
+		Request createdRequest = requestRepository.save(request);
+		
+		assertThat(createdRequest.getId()).isEqualTo(1L);
 	}
+	
+	@Test
+	public void updateTest() {
+		
+		User owner = new User();
+		owner.setId(6L);
+		Request request = new Request(2L, "Novo LapTop HP","Pretendo obter um novo LapTop HP com memória RAM de 64 GB", null, RequestState.OPEN, owner, null);
+		
+		Request updatedRequest = requestRepository.save(request);
+		
+		assertThat(updatedRequest.getId()).isEqualTo("Pretendo obter um novo LapTop HP com memória RAM de 64 GB");
+		
+	}
+	
+	@Test
+	public void getByIdTest() {
+		
+		Optional<Request> result = requestRepository.findById(2L);
+		Request request = result.get();
+		
+		assertThat(request.getSubject()).isEqualTo("Novo LapTop HP");
+		
+	}
+	
+	@Test
+	public void listTest() {
+		
+		List<Request> requests = requestRepository.findAll();
+		assertThat(requests.size()).isEqualTo(1);
+		
+		
+	}
+	
+	@Test
+	public void listByOwnerIdTest() {
+		
+		List<Request> requests = requestRepository.findAllByOwnerId(6L);
+		assertThat(requests.size()).isEqualTo(1);
+		
+		
+	}
+	
+	@Test
+	public void updateStatusTests() {
+		
+		int affectedRows = requestRepository.updateStatus(2L, RequestState.IN_PROGRESS);
+		assertThat(affectedRows).isEqualTo(1);
+		
+	}
+	
+	
+	
 
 }

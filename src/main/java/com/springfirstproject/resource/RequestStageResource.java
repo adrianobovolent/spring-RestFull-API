84f8d5dc1fd2cf49476a7bd8746f260df2e01c85
adrianobovolent <34462906+adrianobovolent@users.noreply.github.com>
2021-06-25 18:19:29 -0300

    Change request mapping to request-stages

--- src/main/java/com/springfirstproject/resource/RequestStageResource.java
+++ src/main/java/com/springfirstproject/resource/RequestStageResource.java
@@ -1,25 +1,20 @@
 package com.springfirstproject.resource;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springfirstproject.domain.Request;
 import com.springfirstproject.domain.RequestStage;
-import com.springfirstproject.service.RequestService;
 import com.springfirstproject.service.RequestStageService;
 
 @RestController
-@RequestMapping(value = "stages")
+@RequestMapping(value = "request-stages")
 public class RequestStageResource {
 	@Autowired private RequestStageService stageService;
 	
